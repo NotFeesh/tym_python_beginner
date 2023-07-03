23d36d826d52f947537e9fd57dbc7c5927f2b96e
Kaiyo <106921666+NotFeesh@users.noreply.github.com>
2023-07-03 02:53:27 -0700

    added break_exercise.py

---
+++ Lesson2/break_exercise.py
@@ -0,0 +1,8 @@
+stations = ["Punggol", "Sengkang", "Buangkok", "Hougang", "Kovan", "Serangoon"]
+
+print("Hi! My name is Albert!")
+
+for station in stations:
+    print(f"I just passed at {station}!")
+
+print(f"I finally arrived at {station}!")
