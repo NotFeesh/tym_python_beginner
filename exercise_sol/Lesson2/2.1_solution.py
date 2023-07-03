4d7487d2db9b57b48093002ff9d75ae181022ea3
Kaiyo <106921666+NotFeesh@users.noreply.github.com>
2023-07-03 03:21:46 -0700

    added exercise 2.1

--- exercise_sol/Lesson2/2.1_solution.py
+++ exercise_sol/Lesson2/2.1_solution.py
@@ -3,5 +3,10 @@ floor_max = int(input("Please enter maximum floor number:"))
 print("Enter 0 to exit")
 while True:
     floor_target = int(input("Please enter the number of the floor you want to go to."))
-    # add loop here
+    if floor_target > floor_max or floor_target < 0:
+        print("That floor doesn't exist!")
+        continue
+    elif floor_target == 0:
+        print("Thank you for using the elevator!")
+        break
     print(f"Travelling to floor {floor_target}.")
