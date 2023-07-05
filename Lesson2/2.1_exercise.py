c1cbb51a57dd13f8605dac4ed154684a6f758c45
Kaiyo <106921666+NotFeesh@users.noreply.github.com>
2023-07-05 01:56:24 -0700

    Update 2.1_exercise.py

--- Lesson2/2.1_exercise.py
+++ Lesson2/2.1_exercise.py
@@ -3,5 +3,5 @@ floor_max = int(input("Please enter maximum floor number:"))
 print("Enter 0 to exit")
 while True:
     floor_target = int(input("Please enter the number of the floor you want to go to."))
-    # add loop here
+    # add statement here
     print(f"Travelling to floor {floor_target}.")
