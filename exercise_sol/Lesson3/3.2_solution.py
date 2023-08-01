6894b59c2ab6ac79a0bd1ebdefddf6ddb3bc607a
Kaiyo <106921666+NotFeesh@users.noreply.github.com>
2023-07-31 18:23:54 -0700

    fixed some errors

--- exercise_sol/Lesson3/3.2_solution.py
+++ exercise_sol/Lesson3/3.2_solution.py
@@ -11,19 +11,18 @@ def guess_the_number():
     # Allow 5 chances to guess the number
     for attempt in range(1, 6):
         # Ask the user to input their guess
-        guess = int(input(f"Attempt {attempt}: Enter your guess: "))
+        guess = int(input(f"Attempt {attempt + 1}: Enter your guess: "))
 
         # Check if the guess matches the secret number
         if guess == secret_number:
-            print(f"Congratulations! You guessed the correct number {secret_number}.")
+            print(f"Congratulations! You guessed the correct number after {attempt} attempts!")
             break
         elif guess < secret_number:
             print("Too low!")
         else:
             print("Too high!")
-
-    else:
-        print(f"Sorry, you've used all 5 chances. The secret number was {secret_number}.")
+    
+    print(f"Sorry, you've used all 5 chances. The secret number was {secret_number}.")
 
 if __name__ == "__main__":
     guess_the_number()
