874a32ab6c24a774a204896180fe3a05b535e6f4
Kaiyo <106921666+NotFeesh@users.noreply.github.com>
2023-08-01 06:38:39 -0700

    Added 4.1 exercise and solution

---
+++ Lesson4/4.1_exercise.py
@@ -0,0 +1,23 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']    
+
+def generate_seating_chart(rows, seats_per_row):
+    seating_chart = []
+    # CODE
+    
+    # You do not have to code anything past this point
+    return seating_chart
+    
+def display_seating_chart(seating_chart):
+    print("Seating Chart:")
+    print("-------------------")
+    for row, seats in enumerate(seating_chart, 1):
+        print(f"Row {alphabet[row - 1]}: {seats}")
+    print("-------------------")
+
+# Get user input
+rows = int(input("Enter the number of rows: "))
+seats_per_row = int(input("Enter the number of seats per row: "))
+
+# Generate and display seating chart
+seating_chart = generate_seating_chart(rows, seats_per_row)
+display_seating_chart(seating_chart)
